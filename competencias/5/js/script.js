c96d1c301ca2cdf6bf0b54c907ae8921c9600ec5
Renato <renatoluizmaia@gmail.com>
2019-09-16 14:58:59 -0300

    competencia 5 finalizada

--- competencias/5/js/script.js
+++ competencias/5/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* USANDO JQUERY */
 
 function abrirJanela(){
@@ -7,7 +6,7 @@ function abrirJanela(){
 	var n2 = 2;
 	var yellowContent  = document.getElementById("ret2");
 	
-	window.alert("testand;o window alert");
+	//*window.alert("testand;o window alert");
 	yellowContent.innerHTML = (n1 + n2) + 1;
 	
 	var texto = document.getElementById("texto");
@@ -23,55 +22,26 @@ function abrirJanela(){
 }
 
 $(document).ready(function(){
-	$(".retangulo").on({
-		
-		mouseenter: function(){
-			$(this).css("background-color","lightgray");
-		},
-		mouseleave: function(){
-			$(this).css("background-color","lightblue");
-		},
-		click: function(){
-			$(this).css("background-color","yellow");
-		}
+
+	/* $("#ret1").click(function(){
+		$(this).hide("slow").slideDown();
+	});
+	$("#ret2").click(function(){
+		$(this).hide("fast").slideUp();
+	});
+	$("#ret3").click(function(){
+		$(this).hide(5000);
+	}); */
+
+	$("button").click(function(){
+		$("#ret1").animate({
+			left:'350px',
+			opacity:'0,9',
+			height:'150',
+			width:'150'
+		});
+
+		$("em").click(function(){$("#ret0").css("background-color","red").slideUp(1500).slideDown(2000);
+	});
 	});
 });
-=======
-/* USANDO JQUERY */
-
-function abrirJanela(){
-	
-	var n1 = 2;
-	var n2 = 2;
-	var yellowContent  = document.getElementById("ret2");
-	
-	window.alert("testand;o window alert");
-	yellowContent.innerHTML = (n1 + n2) + 1;
-	
-	var texto = document.getElementById("texto");
-	
-	var tempo = new Date();
-	var hora = tempo.getHours();
-	
-	if(hora >= 18){
-		texto.innerHTML = "noite";
-	}else{
-		texto.innerHTML = "dia";
-	}
-}
-
-$(document).ready(function(){
-	$(".retangulo").on({
-		
-		mouseenter: function(){
-			$(this).css("background-color","lightgray");
-		},
-		mouseleave: function(){
-			$(this).css("background-color","lightblue");
-		},
-		click: function(){
-			$(this).css("background-color","yellow");
-		}
-	});
-});
->>>>>>> 991c469cce99b1390bd145f31fa3d96755099aaa
